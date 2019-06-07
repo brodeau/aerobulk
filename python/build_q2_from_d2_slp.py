03f4d06542971a6884aedc52a8f719d0968c293a
brodeau <brodeau@gmail.com>
2019-06-07 16:24:35 +0200

    LB: adding some usefull python tools...

---
+++ python/build_q2_from_d2_slp.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+#
+# L. Brodeau, Feb.2001
+
+import sys
+import numpy as nmp
+from netCDF4 import Dataset
+import string
+
+from os.path import basename
+
+import aerobulk_physf as abp
+
+
+iconv=2
+
+if   iconv == 1:
+    cv_d2='D2M'
+    cv_p0='MSL'
+    cv_q2='Q2M'
+    cv_lon = 'lon'
+    cv_lat = 'lat'
+    
+elif iconv == 2:
+    # ERA5...
+    cv_d2='d2m'
+    cv_p0='msl'
+    cv_q2='q2m'
+    cv_lon = 'longitude'
+    cv_lat = 'latitude'
+
+    
+if len(sys.argv) != 2:
+    print 'Usage: '+sys.argv[0]+' <IN_FILE_D2.nc>'
+    sys.exit(0)
+
+cf_d2   = sys.argv[1]
+cf_p0 = string.replace(cf_d2, cv_d2, cv_p0)
+cf_q2 = basename(string.replace(cf_d2, cv_d2, cv_q2))
+print '\n *** Will generate file '+cf_q2+' !\n'
+
+
+# First need time length:
+
+f_d2_in = Dataset(cf_d2)
+vlon     = f_d2_in.variables[cv_lon][:]
+cunt_lon = f_d2_in.variables[cv_lon].units
+clnm_lon = f_d2_in.variables[cv_lon].long_name
+print 'LONGITUDE: ', cunt_lon, clnm_lon
+
+# Extracting the longitude 1D array:
+vlat     = f_d2_in.variables[cv_lat][:]
+cunt_lat = f_d2_in.variables[cv_lat].units
+clnm_lat = f_d2_in.variables[cv_lat].long_name
+print 'LATITUDE: ', cunt_lat, clnm_lat
+
+# Extracting time 1D array:
+vtime     = f_d2_in.variables['time'][:] ; cunt_time = f_d2_in.variables['time'].units
+print 'TIME: ', cunt_time, '\n'
+f_d2_in.close()
+
+
+
+
+
+Nt = len(vtime)
+
+
+print 'Nt = ', Nt
+
+for jt in range(Nt):
+
+    print ' *** jt = ', jt
+    
+        
+    # D2M
+    # ~~~
+    if jt == 0:
+        f_d2_in = Dataset(cf_d2)
+        cunt_d2 = f_d2_in.variables[cv_d2].units
+    xd2     = f_d2_in.variables[cv_d2][jt,:,:]
+    if jt == Nt-1: f_d2_in.close()
+    
+    
+    # MSL
+    # ~~~
+    if jt == 0:
+        f_p0_in = Dataset(cf_p0)
+        cunt_p0 = f_p0_in.variables[cv_p0].units
+    xp0     = f_p0_in.variables[cv_p0][jt,:,:]
+    if jt == Nt-1: f_p0_in.close()
+                
+    
+    # Checking dimensions
+    # ~~~~~~~~~~~~~~~~~~~
+    if jt == 0:
+        dim_d2 = xd2.shape ; dim_p0 = xp0.shape
+        if dim_d2 != dim_p0:
+            print 'Shape problem!!!'; print dim_d2 , dim_p0
+        print '\n'
+        [ nj, ni ] = dim_d2
+        print 'ni, nj, nt = ', ni, nj, Nt
+        xq2 = nmp.zeros(nj*ni) ; xq2.shape = dim_d2
+    
+    
+    # Building q2
+    # ~~~~~~~~~~~
+    xq2 = abp.q_air_dp(xd2, xp0)               
+    
+    # Creating output file
+    # ~~~~~~~~~~~~~~~~~~~~
+    if jt == 0:
+        f_out = Dataset(cf_q2, 'w', format='NETCDF4')
+    
+        # Dimensions:
+        f_out.createDimension(cv_lon, ni)
+        f_out.createDimension(cv_lat, nj)
+        f_out.createDimension('time', None)
+    
+        # Variables
+        id_lon = f_out.createVariable(cv_lon,'f4',(cv_lon,),              zlib=True)
+        id_lat = f_out.createVariable(cv_lat,'f4',(cv_lat,),              zlib=True)
+        id_tim = f_out.createVariable('time','f4',('time',)               zlib=True)
+        id_q2  = f_out.createVariable(cv_q2, 'f4',('time',cv_lat,cv_lon,) zlib=True)
+    
+        # Attributes
+        id_tim.units = cunt_time
+    
+        id_lat.long_name     = clnm_lat
+        id_lat.units         = cunt_lat
+        #id_lat.standard_name = csnm_lat
+    
+        id_lon.long_name     = clnm_lon
+        id_lon.units         = cunt_lon
+        #id_lon.standard_name = csnm_lon
+    
+        id_tim.units         = cunt_time
+    
+        id_q2.long_name = 'Surface specific humidity at 2m, built from '+cv_d2+' and '+cv_p0
+        id_q2.units = 'kg/kg'
+        id_q2.code  = '133'
+        id_q2.table = '128'
+    
+        f_out.About = 'Created by BaraKuda using '+cv_p0+' and '+cv_d2+'. [https://github.com/brodeau/barakuda]'
+    
+        # Filling variables:
+        id_lat[:] = vlat[:]
+        id_lon[:] = vlon[:]
+        
+    id_tim[jt]     = vtime[jt]
+    id_q2[jt,:,:]  = xq2[:,:] 
+    
+    if jt == Nt-1: f_out.close()    
+        
+print 'Bye!'
