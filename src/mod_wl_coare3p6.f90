58a67c92644777527b1b72a6eb0f45c74a050c37
brodeau <brodeau@gmail.com>
2019-09-02 18:22:14 +0200

    LB

--- src/mod_wl_coare3p6.f90
+++ src/mod_wl_coare3p6.f90
@@ -23,7 +23,7 @@ MODULE mod_wl_coare3p6
    IMPLICIT NONE
    PRIVATE
 
-   PUBLIC :: WL_COARE3P6
+   PUBLIC :: WL_COARE3P6, WL_COARE3P6_2
 
    REAL(wp), PARAMETER :: rich   = 0.65_wp   !: critical Richardson number
    REAL(wp), PARAMETER :: z_sst  = 1._wp     !: depth at which bulk SST is taken...
@@ -254,8 +254,255 @@ CONTAINS
 
       !l_wl_c36_never_called = .FALSE.
 
-   END SUBROUTINE WL_COARE3p6
+   END SUBROUTINE WL_COARE3P6
 
+
+
+
+
+
+
+   
+   SUBROUTINE WL_COARE3P6_2( pQsw, pQnsol, pTau, pSST, pdT, pTau_ac, pQ_ac, plon, isd, rdt, &
+      &                    Hwl )
+      !!---------------------------------------------------------------------
+      !!
+      !!  Cool-Skin Warm-Layer scheme according to COARE 3.6 (Fairall et al, 2019)
+      !!     ------------------------------------------------------------------
+      !!
+      !!  **   INPUT:
+      !!
+      !!     *pQsw*       net solar radiative flux to the ocean
+      !!     *pQnsol*     net non-solar heat flux to the ocean
+      !!     *pTau*       surface wind stress
+      !!     *pSST*       SST
+      !!
+      !!   **  INPUT/OUTPUT:
+      !!     *pdT*  :
+      !!
+      !!------------------------------------------------------------------
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)    :: pQsw     ! net solar radiation into the sea [W/m^2]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)    :: pQnsol   ! net non-solar heat flux into the sea [W/m^2]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)    :: pTau     ! wind stress [N/m^2]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)    :: pSST     ! bulk SST at depth z_sst [K]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(inout) :: pdT      ! dT due to warming at depth of pSST such that pSST_true = pSST + pdT
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(inout) :: pTau_ac  ! time integral / accumulated momentum Tauxdt => [N.s/m^2] (reset to zero every midnight)
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(inout) :: pQ_ac    ! time integral / accumulated heat stored by the warm layer Qxdt => [J/m^2] (reset to zero every midnight)
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(inout) :: plon     ! longitude ! lolo
+      INTEGER ,                     INTENT(in)    :: isd      ! current UTC time, counted in second since 00h of the current day
+      REAL(wp),                     INTENT(in)    :: rdt      ! time step in seconds
+      !!
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(out), OPTIONAL :: Hwl    ! depth of warm layer
+      !
+      !
+      INTEGER :: ji,jj
+      !
+      REAL(wp) :: dT_wl, dz_wl, dtime, Qabs, zfs
+      REAL(wp) :: zqac
+      REAL(wp) :: Al, qjoule
+      REAL(wp) :: ctd1, ctd2
+
+      INTEGER  :: jl
+      INTEGER  :: jamset
+      
+      !! INITIALIZATION:
+
+      !pTau=Bx(2)  !stress
+      !!Qsen=Bx(3)   !sensible heat flux
+      !!Qlat=Bx(7)   !latent heat flux - Webb corrected
+      !!Qrain=Bx(34)  !rain heat flux
+      ! => pQnsol == Qlw+Qsen+Qlat+Qrain
+      !dT_skin=Bx(17)    !cool skin
+
+      !pQ_ac   = 0._wp       ! accumulates heat from integral
+      !pTau_ac = 0._wp     ! accumulates stress from integral
+
+      ! Just in case not initialized to 0...
+      !IF ( l_wl_c36_never_called ) THEN
+      !   pTau_ac = 0.
+      !   pQ_ac   = 0.
+      !END IF
+
+      pdT = 0._wp         ! dT initially set to 0._wp
+
+      dT_wl = 0._wp       ! total warming (amplitude) in warm layer
+      dz_wl = dz_max      ! initial depth set to max value
+      Qabs  = 0._wp        ! total heat absorped in warm layer
+      zfs   = 0.5_wp         ! initial value of solar flux absorption
+
+      DO jj = 1, jpj
+         DO ji = 1, jpi
+
+            !PRINT *, ' *** jj =', jj
+            
+            !! Need to know the local solar time from longitude and isd
+            !! *********************************************************
+            
+            !! Hours and minutes in file are supposed to be UTC time:                                                                                       
+            itlag_s = INT( (360. - rlon)/15.*3600. )
+            !PRINT *, ' Lag in sec / hours for solar time =', itlag_s, REAL(itlag_s,wp)/3600._wp                                                            
+            isecday_n = ihh*3600 + imm*60
+            IF ( isecday_n < itlag_s ) THEN
+               isecday_n = isecday_n - itlag_s + 24.*3600.
+            ELSE
+               isecday_n = isecday_n - itlag_s
+            END IF
+            ihh_s = isecday_n/3600
+            imm_s = MOD(isecday_n,3600)/60
+
+            
+
+
+
+            jamset = 0
+            !jump = 1
+
+            !**********************************************************
+            !******************  setup read data loop  ****************
+
+
+
+            !*****  variables for warm layer  ***
+            Al   = 2.1e-5*(pSST(ji,jj)-rt0 + 3.2_wp)**0.79
+            ctd1 = SQRT(2._wp*rich*rCp0_w/(Al*grav*rho0_w))        !mess-o-constants 1
+            ctd2 = SQRT(2._wp*Al*grav/(rich*rho0_w))/(rCp0_w**1.5) !mess-o-constants 2
+
+            !********************************************************
+            !****  Compute apply warm layer  correction *************
+            !********************************************************
+
+            !intime  = yd-fix(yd)
+            !loc     = (lonx+7.5)/15
+            !chktime = loc+intime*24
+            !IF (chktime>24) chktime = chktime-24
+            !it_n = (chktime-24*fix(chktime/24))*3600
+
+            !IF (icount>1) THEN                                  !not first time thru
+
+            !IF ( (it_n <= 21600).OR.(jump == 0) ) THEN  ! (21600 == 6am)
+
+            !   jump = 0
+            
+            IF (it_n < it_b) THEN    !re-zero at midnight               
+               PRINT *, 'LOLO: MIDNIGHT RESET !!!!, it_b,  it_n =>', it_b, it_n
+               jamset         = 0
+               zfs            = 0.5_wp
+               dz_wl          = dz_max
+               pTau_ac(ji,jj) = 0._wp
+               pQ_ac(ji,jj)   = 0._wp
+               dT_wl          = 0._wp
+            END IF
+            
+            IF ( it_n >= 21600 ) THEN  ! (21600 == 6am)
+               
+               PRINT *, 'LOLO: WE DO WL !!!!'
+               PRINT *, ' it_b,  it_n =>',  it_b,  it_n
+               PRINT *, 'pTau, pSST, pdT =', pTau(ji,jj), pSST(ji,jj), pdT(ji,jj)
+               
+               !************************************
+               !****   set warm layer constants  ***
+               !************************************
+
+               dtime = it_n - it_b      ! delta time for integrals
+              
+               Qabs = zfs*pQsw(ji,jj) - pQnsol(ji,jj)       ! tot heat absorbed in warm layer
+
+               PRINT *, 'dtime,  pQsw, pQnsol, Qabs =', dtime,  pQsw(ji,jj), pQnsol(ji,jj), Qabs
+               
+               IF ( (Qabs >= Qabs_thr).OR.(jamset == 1) ) THEN         ! Check for threshold
+                  
+                  PRINT *, ' pTau_ac, pQ_ac =', pTau_ac(ji,jj), pQ_ac(ji,jj)
+
+                  jamset = 1                                         ! indicates threshold crossed
+                  pTau_ac(ji,jj) = pTau_ac(ji,jj) + MAX(.002_wp , pTau(ji,jj))*dtime      ! momentum integral
+                  
+                  IF ( pQ_ac(ji,jj) + Qabs*dtime > 0._wp ) THEN         !check threshold for warm layer existence
+                     !******************************************
+                     ! Compute the absorption profile
+                     !******************************************
+                     DO jl = 1, 5                           !loop 5 times for zfs
+                        zfs = 1. - ( 0.28*0.014*(1. - EXP(-dz_wl/0.014)) + 0.27*0.357*(1. - EXP(-dz_wl/0.357)) &
+                           &        + 0.45*12.82*(1-EXP(-dz_wl/12.82)) ) / dz_wl
+                        qjoule = (zfs*pQsw(ji,jj) - pQnsol(ji,jj))*dtime
+                        zqac = pQ_ac(ji,jj) + qjoule
+                        IF (zqac > 0._wp)  dz_wl = MIN( dz_max , ctd1*pTau_ac(ji,jj)/SQRT(zqac)) !Compute warm-layer depth
+                     END DO
+
+                  ELSE
+                     !***********************
+                     ! Warm layer wiped out
+                     !***********************
+                     zfs    = 0.75
+                     dz_wl  = dz_max
+                     qjoule = (zfs*pQsw(ji,jj) - pQnsol(ji,jj))*dtime
+                     zqac   = pQ_ac(ji,jj) + qjoule
+                  END IF
+
+                  pQ_ac(ji,jj) = zqac !heat integral
+
+                  !*******  compute dt_warm  ******
+                  !LOLOnew: dT_wl = ctd2*MAX(0._wp,pQ_ac(ji,jj))**1.5/pTau_ac(ji,jj)                  
+                  IF (pQ_ac(ji,jj) > 0._wp) THEN
+                     dT_wl = ctd2*pQ_ac(ji,jj)**1.5/pTau_ac(ji,jj)
+                  ELSE
+                     dT_wl = 0._wp
+                  END IF
+
+               END IF ! IF ( (Qabs>=50).OR.(jamset==1) )
+
+
+               IF (dz_wl < z_sst) THEN           !Compute warm layer correction
+                  pdT(ji,jj) = dT_wl
+               ELSE
+                  pdT(ji,jj) = dT_wl*z_sst/dz_wl
+               END IF
+
+            END IF ! IF ( it_n >= 21600 ) THEN  ! (21600 == 6am)
+            
+            !END IF !  IF ( (it_n<=21600).OR.(jump==0) )  end 6am start first time thru
+
+
+            !PRINT *, ' *** END => pdT =', pdT(ji,jj)
+
+            IF ( PRESENT(Hwl) ) Hwl(ji,jj) = dz_wl
+
+         END DO
+         PRINT *, ''
+      END DO
+      
+      !END IF  !  IF (icount>1)
+
+
+      !it_b = it_n
+
+      !************* output from routine  *****************************
+      !Output:   A=[usr tau hsb hlb hbb hsbb hlwebb tsr qsr zo  zot zoq  Cd  Ch  Ce  L zet dT_skinx dqerx tkt Urf Trf Qrf RHrf UrfN Qlw  Le rhoa UN U10 U10N Cdn_10 Chn_10 Cen_10 RF Qs Evap T10 Q10 RH10 ug Whf Edis]
+      !              1   2   3   4   5    6     7    8   9  10  11   12  13  14  15 16  17   18   19    20  21  22  23  24   25   26  27  28  29  30  31    32     33     34   35 36  37   38  39  40  41  42  43
+      !pTs = pSST + pdT
+      !Bx = coare36vn_zrf(u,zu(ibg),t,zt(ibg),rh,zq(ibg),P,ts,Rs,Rl,latx,zi,rain,Ss(ibg),cp(ibg),sigH(ibg),zrf_u(ibg),zrf_t(ibg),zrf_q(ibg))
+      !pTau = Bx(2)            !hold stress
+      !Qsen = Bx(3)             !hold shf
+      !Qlat = Bx(4)             !hold lhf - use Webb corrected value
+      !dT_skin = Bx(17)
+      !Qrain = Bx(34)            !hold rain flux
+
+      !B(ibg,1) = dT_wl   ! warming across entire warm layer deg.C
+      !B(ibg,2) = dz_wl   ! warm layer thickness m
+      !B(ibg,3) = pdT     ! heating at selected depth
+
+      !icount = icount+1
+
+
+      !l_wl_c36_never_called = .FALSE.
+
+   END SUBROUTINE WL_COARE3P6_2
+
+
+
+
+
+
+   
 END MODULE mod_wl_coare3p6
 
 !**************************************************
