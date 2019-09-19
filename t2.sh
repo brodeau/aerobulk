0666f83447ef97efcf2e23dfd0faf66b625df293
brodeau <brodeau@gmail.com>
2019-09-19 16:30:01 +0200

    LB

--- t2.sh
+++ t2.sh
@@ -4,9 +4,15 @@
 
 fout="out_test_aerobulk_buoy_ecmwf2.out"
 
+
+
+DSTOR="/data/gcm_setup/STATION_ASF/STATION_ASF-I"
+
+if [ `hostname` = "lacroix" ]; then DSTOR="/data1/laurent/STATION_ASF/STATION_ASF-I" ; fi
+
 rm -f lolo_ecmwf2.nc ${fout}
 
-./bin/test_aerobulk_buoy_series_skin.x -f /data/gcm_setup/STATION_ASF/STATION_ASF-I/Station_PAPA_50N-145W_2012-2012.nc4 1>${fout} <<EOF
+./bin/test_aerobulk_buoy_series_skin.x -f ${DSTOR}/Station_PAPA_50N-145W_2012-2012.nc4 1>${fout} <<EOF
 2
 14
 14
