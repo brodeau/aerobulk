6225186c58931bd3b5ff9fba023c74cc5a11d034
Laurent Brodeau <brodeau@ige-template-domOK.lthe.local>
2020-02-11 10:44:53 +0100

    LB

--- misc/build_era5_1d_forcing_arctic.sh
+++ misc/build_era5_1d_forcing_arctic.sh
@@ -5,8 +5,9 @@ DIR_IN="/home/datawork-lops-drakkarcom/DATA-REFERENCE/ERA5-FORCING/ROOT-FILES"
 
 fo="ERA5_station_arctic_1h_${YEAR}.nc"
 
-ip=1224
-jp=18
+coord="85.5 N, -54.0 W"
+ip=1224 ; clon="306.0"
+jp=18   ; clat="85.5"
 
 # istl1
 DIR_IN2="/mnt/meom/workdir/brodeau/ECMWF/ERA5-T720/ARCTIC/build"
@@ -44,39 +45,46 @@ if [ `hostname` = "ige-meom-cal1" ]; then
 
 fi
 
-for cv in "msl" "d2m" "t2m" "u10" "v10" "ssrd" "strd"; do
-    echo
-    if [ ! -f ./${cv}_era5.tmp ]; then
-        #
-        f_in=${DIR_IN}/${YEAR}/ERA5_${cv}_y${YEAR}.nc
-        CMD="ncks -F -O --no-abc -h -d longitude,${ip},`expr ${ip} + 2` -d latitude,${jp},`expr ${jp} + 2` ${f_in} -o ${cv}0.tmp"
-        echo; echo "${CMD}"; ${CMD}; echo
-        #
-        CMD="ncpdq -O -U ${cv}0.tmp -o ${cv}.tmp"
-        echo; echo "${CMD}"; ${CMD}; echo
-        #
-        ncecat -O ${cv}.tmp ${cv}_era5.tmp     # Add degenerate record dimension named "record"
-        ncpdq -O -a time,record ${cv}_era5.tmp ${cv}_era5.tmp # Switch "record" and "time"
-        ncwa -O -a record ${cv}_era5.tmp ${cv}_era5.tmp       # Remove (degenerate) "record"
-        #
-        CMD="ncks -4 -L 5 --cnk_dmn longitude,3 --cnk_dmn latitude,1 --cnk_dmn time,1 ${cv}_era5.tmp -o ${cv}_era5.nc4"
-        echo; echo "${CMD}"; ${CMD}; echo
-        rm -f ${cv}.tmp ${cv}0.tmp ${cv}_era5.tmp
-        #
-    else
-        echo " ${cv}_era5.tmp is already here!"
-    fi
-    echo
-done
 
+if [ `hostname` != "ige-meom-cal1" ]; then
+
+    for cv in "msl" "d2m" "t2m" "u10" "v10" "ssrd" "strd"; do
+        echo
+        if [ ! -f ./${cv}_era5.tmp ]; then
+            #
+            f_in=${DIR_IN}/${YEAR}/ERA5_${cv}_y${YEAR}.nc
+            CMD="ncks -F -O --no-abc -h -d longitude,${ip},`expr ${ip} + 2` -d latitude,${jp},`expr ${jp} + 2` ${f_in} -o ${cv}0.tmp"
+            echo; echo "${CMD}"; ${CMD}; echo
+            #
+            CMD="ncpdq -O -U ${cv}0.tmp -o ${cv}.tmp"
+            echo; echo "${CMD}"; ${CMD}; echo
+            #
+            ncecat -O ${cv}.tmp ${cv}_era5.tmp     # Add degenerate record dimension named "record"
+            ncpdq -O -a time,record ${cv}_era5.tmp ${cv}_era5.tmp # Switch "record" and "time"
+            ncwa -O -a record ${cv}_era5.tmp ${cv}_era5.tmp       # Remove (degenerate) "record"
+            #
+            CMD="ncks -4 -L 5 --cnk_dmn longitude,3 --cnk_dmn latitude,1 --cnk_dmn time,1 ${cv}_era5.tmp -o ${cv}_era5.nc4"
+            echo; echo "${CMD}"; ${CMD}; echo
+            rm -f ${cv}.tmp ${cv}0.tmp ${cv}_era5.tmp
+            #
+        else
+            echo " ${cv}_era5.tmp is already here!"
+        fi
+        echo
+    done
+
+fi
 
 
 if [ `hostname` = "ige-meom-cal1" ]; then
     rm -f ${fo}
 
-    rsync -avP msl_era5.tmp ${fo}
+    rsync -avP msl_era5.nc4 ${fo}
 
-    for cv in "d2m" "t2m" "u10" "v10" "ssrd" "strd"; do
-        ncks -A --no-abc -h -v ${cv} ${cv}_era5.tmp -o ${fo}
+    for cv in "istl1" "skt" "d2m" "t2m" "u10" "v10" "ssrd" "strd"; do
+        ncks -A --no-abc -h -v ${cv} ${cv}_era5.nc4 -o ${fo}
     done
+
+    ncatted -h -O -a About,global,o,c,"Extraction of ERA5 at ${coord} (i=${ip}, j=${jp})" ${fo}
+
 fi
