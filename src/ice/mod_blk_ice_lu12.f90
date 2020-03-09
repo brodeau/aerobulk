290d0a38e526fa2e47675144213da0d8b2ad3ccd
brodeau <brodeau@gmail.com>
2020-03-09 18:25:10 +0100

    LB: general case for LU12, can specify Sc, hf & Di (as optionals)...

--- src/ice/mod_blk_ice_lu12.f90
+++ src/ice/mod_blk_ice_lu12.f90
@@ -20,16 +20,16 @@
 !  at_ip      !: total melt pond concentration
 !  hm_ip      !: mean melt pond depth                     [m]
 !  vt_ip      !: total melt pond volume per gridcell area [m]
-! 
+!
 !
 ! # "h_f" in Lupkes is mean floe freeboard
 !
 ! "ice freeboard" is saved as:
 !    ( zrho1 * hm_i(:,:) - zrho2 * hm_s(:,:) )
 !    WHERE( z2d < 0._wp )   z2d = 0._wp
-! into "icewri.F90"   
+! into "icewri.F90"
 !
-!   possibly "h_f" in Lupkes* 
+!   possibly "h_f" in Lupkes*
 !
 MODULE mod_blk_ice_lu12
    !!====================================================================================
@@ -42,7 +42,9 @@ MODULE mod_blk_ice_lu12
    !!====================================================================================
    USE mod_const       !: physical and other constants
    USE mod_phymbl      !: misc. physical functions
-
+   
+   USE mod_blk_ncar, ONLY: CD_N10_NCAR  !: in order to have a decent estimate of z0 over water
+   
    IMPLICIT NONE
    PRIVATE
 
@@ -53,11 +55,16 @@ MODULE mod_blk_ice_lu12
    REAL(wp), PARAMETER :: rNu_0    = 1._wp
    REAL(wp), PARAMETER :: rMu_0    = 1._wp
    REAL(wp), PARAMETER :: rBeta_0  = 1._wp
+   REAL(wp), PARAMETER :: rhmin_0  = 0.286_wp  ! Eq.(25)
+   REAL(wp), PARAMETER :: rhmax_0  = 0.534_wp  ! Eq.(25)
+   REAL(wp), PARAMETER :: rDmin_0 =   8._wp      ! Eq.(27)
+   REAL(wp), PARAMETER :: rDmax_0 = 300._wp      ! Eq.(27)
    
    !!----------------------------------------------------------------------
 CONTAINS
 
    SUBROUTINE turb_ice_lu12( kt, zt, zu, Ts_i, t_zt, qs_i, q_zt, U_zu, frice, &
+      !&                      hf, Di,                                          &
       &                      Cd, Ch, Ce, t_zu, q_zu, Ub,                      &
       &                      CdN, ChN, CeN, xz0, xu_star, xL, xUN10 )
       !!----------------------------------------------------------------------
@@ -79,6 +86,11 @@ CONTAINS
       !!    *  q_zt : specific humidity of air at zt                          [kg/kg]
       !!    *  U_zu : scalar wind speed at zu                                 [m/s]
       !!    * frice : sea-ice concentration        (fraction)
+      !!
+      !! OPTIONAL INPUT:
+      !! ----------------
+      !!    * hf    : mean freeboard of floes    [m]
+      !!    * Di    : cross wind dimension of the floe (aka effective edge length for form drag)   [m]
       !!
       !! OUTPUT :
       !! --------
@@ -110,39 +122,48 @@ CONTAINS
       REAL(wp), INTENT(in ), DIMENSION(jpi,jpj) :: q_zt  ! spec. air humidity at zt               [kg/kg]
       REAL(wp), INTENT(in ), DIMENSION(jpi,jpj) :: U_zu  ! relative wind module at zu                [m/s]
       REAL(wp), INTENT(in ), DIMENSION(jpi,jpj) :: frice ! sea-ice concentration        (fraction)
+      !!
+      !REAL(wp), INTENT(in ), DIMENSION(jpi,jpj), OPTIONAL :: hf    ! mean freeboard of floes    [m]
+      !REAL(wp), INTENT(in ), DIMENSION(jpi,jpj), OPTIONAL :: Di    ! cross wind dimension of the floe (aka effective edge length for form drag)   [m]
+      !!
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: Cd    ! transfer coefficient for momentum         (tau)
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: Ch    ! transfer coefficient for sensible heat (Q_sens)
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: Ce    ! transfert coefficient for evaporation   (Q_lat)
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: t_zu  ! pot. air temp. adjusted at zu               [K]
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: q_zu  ! spec. humidity adjusted at zu           [kg/kg]
       REAL(wp), INTENT(out), DIMENSION(jpi,jpj) :: Ub ! bulk wind speed at zu                     [m/s]
+      !!
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: CdN
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: ChN
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: CeN
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: xz0  ! Aerodynamic roughness length   [m]
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: xu_star  ! u*, friction velocity
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: xL  ! zeta (zu/L)
+      REAL(wp), INTENT(out), DIMENSION(jpi,jpj), OPTIONAL :: xUN10  ! Neutral wind at zu
       !!----------------------------------------------------------------------------------
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   CdN
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   ChN
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   CeN
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   xz0  ! Aerodynamic roughness length   [m]
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   xu_star  ! u*, friction velocity
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   xL  ! zeta (zu/L)
-      REAL(wp), INTENT(  out), OPTIONAL, DIMENSION(jpi,jpj) ::   xUN10  ! Neutral wind at zu
+      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: dt_zu, dq_zu, z0_w
       !!
-      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: dt_zu, dq_zu
-      !!
-      LOGICAL ::  lreturn_cdn=.FALSE., lreturn_chn=.FALSE., lreturn_cen=.FALSE., &
-         &        lreturn_z0=.FALSE., lreturn_ustar=.FALSE., lreturn_L=.FALSE., lreturn_UN10=.FALSE.
+      LOGICAL :: l_known_hf=.FALSE., l_known_Di=.FALSE.
+      LOGICAL :: lreturn_cdn=.FALSE., lreturn_chn=.FALSE., lreturn_cen=.FALSE., &
+         &       lreturn_z0=.FALSE., lreturn_ustar=.FALSE., lreturn_L=.FALSE., lreturn_UN10=.FALSE.
       !!
       CHARACTER(len=40), PARAMETER :: crtnm = 'turb_ice_lu12@mod_blk_ice_lu12.f90'
       !!----------------------------------------------------------------------------------
+      !l_known_hf    = PRESENT(hf)
+      !l_known_Di    = PRESENT(Di)
+
+      lreturn_cdn   = PRESENT(CdN)
+      lreturn_chn   = PRESENT(ChN)
+      lreturn_cen   = PRESENT(CeN)
+      lreturn_z0    = PRESENT(xz0)
+      lreturn_ustar = PRESENT(xu_star)
+      lreturn_L     = PRESENT(xL)
+      lreturn_UN10  = PRESENT(xUN10)
+
+
       !u_star(jpi,jpj), t_star(jpi,jpj), q_star(jpi,jpj),  &
-      ALLOCATE ( dt_zu(jpi,jpj),  dq_zu(jpi,jpj) )
-      
-      IF( PRESENT(CdN) )     lreturn_cdn   = .TRUE.
-      IF( PRESENT(ChN) )     lreturn_chn   = .TRUE.
-      IF( PRESENT(CeN) )     lreturn_cen   = .TRUE.
-      IF( PRESENT(xz0) )     lreturn_z0    = .TRUE.
-      IF( PRESENT(xu_star) ) lreturn_ustar = .TRUE.
-      IF( PRESENT(xL) )      lreturn_L     = .TRUE.
-      IF( PRESENT(xUN10) )   lreturn_UN10  = .TRUE.
-      
+      ALLOCATE ( dt_zu(jpi,jpj), dq_zu(jpi,jpj) , z0_w(jpi,jpj) )
+
       !! Scalar wind speed cannot be below 0.2 m/s
       Ub = MAX( U_zu, wspd_thrshld_ice )
 
@@ -160,13 +181,23 @@ CONTAINS
       !!  we use the method that comes in LG15, i.e. by starting from a default roughness length z0 for skin drag:
 
       Ce(:,:) = rz0_s_0 !! temporary array to contain roughness length for skin drag !
-      !!
-      Cd(:,:) = Cd_from_z0( zu, Ce(:,:) )  + CdN10_form_LU12( frice(:,:) )
+
+      
+      !Cd(:,:) = Cd_from_z0( zu, Ce(:,:) )  + CdN10_form_LU12( frice(:,:) )
+      !PRINT *, 'LOLO: estimate of Cd_f_i method #1 =>', CdN10_form_LU12( frice(:,:) ); PRINT *, ''
+      
+      !! We need an estimate of z0 over water:
+      z0_w(:,:) = z0_from_Cd( zu, CD_N10_NCAR(Ub) )
+      !PRINT *, 'LOLO: estimate of z0_w =>', z0_w      
+      Cd(:,:)   = Cd_from_z0( zu, Ce(:,:) )  + CdN10_f_LU12( frice(:,:), z0_w(:,:) )
       !!          N10 skin drag                     N10 form drag
 
+      !PRINT *, 'LOLO: estimate of Cd_f_i method #2 =>', CdN10_f_LU12( frice(:,:), z0_w(:,:) )
+
+      
       Ch(:,:) = Cd(:,:)
       Ce(:,:) = Cd(:,:)
-      
+
 
       IF( lreturn_cdn )   CdN = Cd(:,:)
       IF( lreturn_chn )   ChN = Ch(:,:)
@@ -177,13 +208,13 @@ CONTAINS
       IF( lreturn_L )     xL      = 1./One_on_L(t_zu, q_zu, SQRT(Cd)*Ub, &
          &                          Cd/SQRT(Cd)*dt_zu, Cd/SQRT(Cd)*dq_zu)
       IF( lreturn_UN10 )  xUN10   = SQRT(Cd)*Ub/vkarmn * LOG( 10._wp / z0_from_Cd( zu, Cd ) )
-      
-      DEALLOCATE ( dt_zu, dq_zu )
+
+      DEALLOCATE ( dt_zu, dq_zu, z0_w )
 
    END SUBROUTINE turb_ice_lu12
 
 
-   
+
    FUNCTION CdN10_form_LU12( pfrice )
       !!----------------------------------------------------------------------
       !!                      ***  ROUTINE  CdN10_form_LU12  ***
@@ -223,15 +254,91 @@ CONTAINS
       REAL(wp)            ::   zcoef
       !!----------------------------------------------------------------------
       zcoef = rNu_0 + 1._wp / ( 10._wp * rBeta_0 )
-      
+
       !! We are not an AGCM, we are an OGCM!!! => we drop term "(1 - A)*Cd_w"
       !!  => so we keep only the last rhs terms of Eq.(1) of Lupkes et al, 2013 that we divide by "A":
-      
+      !! (we multiply Cd_i_s and Cd_i_f by A later, when applying ocean-ice partitioning...
+
       CdN10_form_LU12(:,:) = rCe_0 * pfrice(:,:)**(rMu_0 - 1._wp) * (1._wp - pfrice(:,:))**zcoef
-      
       !! => seems okay for winter 100% sea-ice as second rhs term vanishes as pfrice == 1....
-      
+
    END FUNCTION CdN10_form_LU12
+
+
+
+   FUNCTION CdN10_f_LU12( pfrice, pz0w,  pSc, phf, pDi  )
+      !!----------------------------------------------------------------------
+      !!                      ***  ROUTINE  CdN10_f_LU12  ***
+      !!
+      !!        GENERAL FORM OF EQUATION 22 of Lupkes et al. 2012
+      !!        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+      !!
+      !! ** Purpose :    Computes the "form" contribution of the neutral air-ice
+      !!                 drag referenced at 10m to make it dependent on edges at
+      !!                 leads, melt ponds and flows (to be added to the "skin"
+      !!                 contribution. After some
+      !!                 approximations, this can be resumed to a dependency on
+      !!                 ice concentration.
+      !!
+      !! ** References : Lupkes et al. JGR 2012 (theory)
+      !!
+      !!----------------------------------------------------------------------
+      REAL(wp), DIMENSION(jpi,jpj)                       :: CdN10_f_LU12  ! neutral FORM drag coefficient contribution over sea-ice
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)           :: pfrice ! ice concentration [fraction]  => at_i_b  ! NOT USED if pSc, phf and pDi all provided...
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in)           :: pz0w   ! roughness length over water  [m]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in), OPTIONAL :: pSc    ! shletering function [0-1] (Sc->1 for large distance between floes, ->0 for small distances)
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in), OPTIONAL :: phf    ! mean freeboard of floes    [m]
+      REAL(wp), DIMENSION(jpi,jpj), INTENT(in), OPTIONAL :: pDi    ! cross wind dimension of the floe (aka effective edge length for form drag)   [m]
+      !!----------------------------------------------------------------------
+      LOGICAL :: l_known_Sc=.FALSE., l_known_hf=.FALSE., l_known_Di=.FALSE.
+      REAL(wp) :: ztmp, zrlog, zfri, zfrw, zSc, zhf, zDi
+      INTEGER  :: ji, jj
+      !!----------------------------------------------------------------------
+      l_known_Sc    = PRESENT(pSc)
+      l_known_hf    = PRESENT(phf)
+      l_known_Di    = PRESENT(pDi)
+
+      DO jj = 1, jpj
+         DO ji = 1, jpi
+
+            zfri = pfrice(ji,jj)
+            zfrw = (1._wp - zfri)
+            
+            IF(l_known_Sc) THEN
+               zSc = pSc(ji,jj)
+            ELSE
+               !! Sc parameterized in terms of A (ice fraction):
+               zSc = zfrw**(1._wp / ( 10._wp * rBeta_0 ))   ! Eq.(31)
+               PRINT *, 'LOLO: Sc PARAMETERIZED !!! =>', zSc
+            END IF
+            
+            IF(l_known_hf) THEN
+               zhf = phf(ji,jj)
+            ELSE
+               !! hf parameterized in terms of A (ice fraction):
+               zhf = rhmax_0*zfri + rhmin_0*zfrw  ! Eq.(25)
+               PRINT *, 'LOLO: hf PARAMETERIZED !!! =>', zhf
+            END IF
+            
+            IF(l_known_Di) THEN
+               zDi = pDi(ji,jj)
+            ELSE
+               !! Di parameterized in terms of A (ice fraction):
+               ztmp = 1._wp / ( 1._wp - (rDmin_0/rDmax_0)**(1._wp/rBeta_0) )   ! A* Eq.(27)
+               zDi =  rDmin_0 * ( ztmp/(ztmp - zfri) )**rBeta_0                !    Eq.(26)
+               PRINT *, 'LOLO: Di PARAMETERIZED !!! =>', zDi
+            END IF
+            
+            ztmp  = 1._wp/pz0w(ji,jj)
+            zrlog = LOG(zhf*ztmp) / LOG(10._wp*ztmp)
+
+
+            CdN10_f_LU12(:,:) = 0.5_wp* 0.3_wp * zrlog*zrlog * zSc*zSc * zhf/zDi   ! Eq.(22)
+            !!                   1/2      Ce
+
+         END DO
+      END DO
+   END FUNCTION CdN10_f_LU12
 
 
    !!======================================================================
