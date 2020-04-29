dfc6bbcc5181db0643ea402a6c90316cc38f1c37
brodeau <brodeau@gmail.com>
2020-04-29 15:22:25 +0200

    LB

--- python/plot_tests/plot_Cx_wind.py
+++ python/plot_tests/plot_Cx_wind.py
@@ -157,14 +157,14 @@ for jtv in range(ntv):
     #vrt_u[jtv] = float(vtv_u[jtv])/100. # dTv as a float
     for ja in range(nb_algo):
         xun_u[:,:,jtv,ja] = read_ascii_column(cdir_in+'/un_dtv_'+vtv_u[jtv]+'_sst_'+csst+'_'+valgo_nm[ja]+'.dat', [0,1])
-        xun_u[1,:,jtv,ja] = xun_u[1,:,jtv,ja] - xun_u[0,:,jtv,ja] ; # => UN10 - U
+        xun_u[1,:,jtv,ja] = xun_u[1,:,jtv,ja] ; # => UN10
 
 xun_s = nmp.zeros((2,nU,ntv,nb_algo))
 for jtv in range(ntv):
     #vrt_s[jtv] = float(vtv_s[jtv])/100. # dTv as a float
     for ja in range(nb_algo):
         xun_s[:,:,jtv,ja] = read_ascii_column(cdir_in+'/un_dtv_'+vtv_s[jtv]+'_sst_'+csst+'_'+valgo_nm[ja]+'.dat', [0,1])
-        xun_s[1,:,jtv,ja] = xun_s[1,:,jtv,ja] - xun_s[0,:,jtv,ja] ; # => UN10 - U
+        xun_s[1,:,jtv,ja] = xun_s[1,:,jtv,ja]; # => UN10
 
 ##### u* ####
 xus_u = nmp.zeros((2,nU,ntv,nb_algo))
@@ -275,7 +275,7 @@ vU[:] = xcd_u[0,:,jtv,0]
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-def plot_Cx( nvt, nba, cvar_nm, vu10, xCx, vstab, valg_dn, w_min, w_max, dw, cx_min, cx_max, dcx, istab=0, cunit='' ):
+def plot_var_u( nvt, nba, cvar_nm, vu10, xF, vstab, valg_dn, w_min, w_max, dw, cx_min, cx_max, dcx, istab=0, cunit='' ):
     #
     cstblt = 'unstable'
     csgn   = '-'
@@ -289,7 +289,7 @@ def plot_Cx( nvt, nba, cvar_nm, vu10, xCx, vstab, valg_dn, w_min, w_max, dw, cx_
     ax1.annotate(cstblt, xy=(0.4, 1.06), xycoords='axes fraction', **font_ttl)
     for jtv in range(nvt):
         for ja in range(nba):
-            ax1.plot( vu10, xCx[1,:,jtv,ja], label=valg_dn[ja]+r' $\Delta\Theta=$'+csgn+str(vstab[jtv])+'K', \
+            ax1.plot( vu10, xF[1,:,jtv,ja], label=valg_dn[ja]+r' $\Delta\Theta=$'+csgn+str(vstab[jtv])+'K', \
                       linewidth=3.5-jtv*0.7, color=vcolor[ja], linestyle=vlines[ja], zorder=0.75 )
     vx_ticks = nmp.arange(w_min, w_max+dw, dw)
     plt.xticks( vx_ticks )
@@ -315,160 +315,109 @@ def plot_Cx( nvt, nba, cvar_nm, vu10, xCx, vstab, valg_dn, w_min, w_max, dw, cx_
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-if not l_multi_fig:
 
-    iplt = plot_Cx( ntv, nb_algo, 'C_D', vU, xcd_u, vrt_s, valgo_DN, U_min, U_max, dU, F_cd_u_min, F_cd_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
-    iplt = plot_Cx( ntv, nb_algo, 'C_D', vU, xcd_s, vrt_s, valgo_DN, U_min, U_max, dU, F_cd_s_min, F_cd_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_D', vU, xcd_u, vrt_s, valgo_DN, U_min, U_max, dU, F_cd_u_min, F_cd_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_D', vU, xcd_s, vrt_s, valgo_DN, U_min, U_max, dU, F_cd_s_min, F_cd_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'C_H', vU, xch_u, vrt_s, valgo_DN, U_min, U_max, dU, F_ch_u_min, F_ch_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
-    iplt = plot_Cx( ntv, nb_algo, 'C_H', vU, xch_s, vrt_s, valgo_DN, U_min, U_max, dU, F_ch_s_min, F_ch_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_H', vU, xch_u, vrt_s, valgo_DN, U_min, U_max, dU, F_ch_u_min, F_ch_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_H', vU, xch_s, vrt_s, valgo_DN, U_min, U_max, dU, F_ch_s_min, F_ch_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'C_E', vU, xce_u, vrt_s, valgo_DN, U_min, U_max, dU, F_ce_u_min, F_ce_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
-    iplt = plot_Cx( ntv, nb_algo, 'C_E', vU, xce_s, vrt_s, valgo_DN, U_min, U_max, dU, F_ce_s_min, F_ce_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_E', vU, xce_u, vrt_s, valgo_DN, U_min, U_max, dU, F_ce_u_min, F_ce_u_max, dCd_u, istab=0, cunit='$10^{-3}$' )
+iplt = plot_var_u( ntv, nb_algo, 'C_E', vU, xce_s, vrt_s, valgo_DN, U_min, U_max, dU, F_ce_s_min, F_ce_s_max, dCd_u, istab=1, cunit='$10^{-3}$' )
 
 
-    iplt = plot_Cx( ntv, nb_algo, 'L',   vU, xlo_u, vrt_s, valgo_DN, U_min, U_max, dU, -500.,   0., 50., istab=0, cunit='[m]' )
-    iplt = plot_Cx( ntv, nb_algo, 'L',   vU, xlo_s, vrt_s, valgo_DN, U_min, U_max, dU,    0., 500., 50., istab=1, cunit='[m]' )
+iplt = plot_var_u( ntv, nb_algo, 'L',   vU, xlo_u, vrt_s, valgo_DN, U_min, U_max, dU, -500.,   0., 50., istab=0, cunit='[m]' )
+iplt = plot_var_u( ntv, nb_algo, 'L',   vU, xlo_s, vrt_s, valgo_DN, U_min, U_max, dU,    0., 500., 50., istab=1, cunit='[m]' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'UN-U',   vU, xun_u, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=0, cunit='[m/s]' )
-    iplt = plot_Cx( ntv, nb_algo, 'UN-U',   vU, xun_s, vrt_s, valgo_DN, U_min, U_max, dU,   -2.,  0., 0.1, istab=1, cunit='[m/s]' )
+#iplt = plot_var_u( ntv, nb_algo, 'UN-U',   vU, xun_u, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=0, cunit='[m/s]' )
+#iplt = plot_var_u( ntv, nb_algo, 'UN-U',   vU, xun_s, vrt_s, valgo_DN, U_min, U_max, dU,   -2.,  0., 0.1, istab=1, cunit='[m/s]' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'u*',   vU, xus_u, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=0, cunit='[m/s]' )
-    iplt = plot_Cx( ntv, nb_algo, 'u*',   vU, xus_s, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=1, cunit='[m/s]' )
+iplt = plot_var_u( ntv, nb_algo, 'u*',   vU, xus_u, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=0, cunit='[m/s]' )
+iplt = plot_var_u( ntv, nb_algo, 'u*',   vU, xus_s, vrt_s, valgo_DN, U_min, U_max, dU,    0.,  2., 0.1, istab=1, cunit='[m/s]' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'Ri_B',   vU, xri_u, vrt_s, valgo_DN, U_min, U_max, dU,   -10.,   0., 1, istab=0, cunit='[]' )
-    iplt = plot_Cx( ntv, nb_algo, 'Ri_B',   vU, xri_s, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  10., 1, istab=1, cunit='[]' )
+iplt = plot_var_u( ntv, nb_algo, 'Ri_B',   vU, xri_u, vrt_s, valgo_DN, U_min, U_max, dU,   -10.,   0., 1, istab=0, cunit='[]' )
+iplt = plot_var_u( ntv, nb_algo, 'Ri_B',   vU, xri_s, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  10., 1, istab=1, cunit='[]' )
 
-    iplt = plot_Cx( ntv, nb_algo, 'z_0',   vU, xz0_u, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  0.005, 0.001, istab=0, cunit='[]' )
-    iplt = plot_Cx( ntv, nb_algo, 'z_0',   vU, xz0_s, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  0.1,   0.01, istab=1, cunit='[]' )
+iplt = plot_var_u( ntv, nb_algo, 'z_0',   vU, xz0_u, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  0.005, 0.001, istab=0, cunit='[]' )
+iplt = plot_var_u( ntv, nb_algo, 'z_0',   vU, xz0_s, vrt_s, valgo_DN, U_min, U_max, dU,     0.,  0.1,   0.01, istab=1, cunit='[]' )
 
-    
-else:
-    
-    fig = plt.figure(num = 1, figsize=(12,14), facecolor='w', edgecolor='k')
-    ax1  = plt.axes([rleft, 2*rheig+3*rbott+rbext, rwidt, rheig])
+
+
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+def plot_scat( nba, cvar_nm, xF1, xF2, valg_dn, f1_rng=None, f2_rng=None, istab=0, cxunit='', cyunit='' ):
+    #
+    cstblt = 'unstable'
+    csgn   = '-'
+    if istab==1:
+        cstblt = 'stable'
+        csgn   = '+'
+    #
+    print 'lolo: shape of xF1 =', nmp.shape(xF1)
+    print 'lolo: shape of xF2 =', nmp.shape(xF2)
+    #
+    #print '\nlolo: X variable for first algo:'
+    #print xF1[1,:,:,0]
+    #sys.exit(0)
+    #
+    fig = plt.figure(num=1, figsize=(14.,10.), facecolor='w', edgecolor='k')
+    ax1 = plt.axes([0.07, 0.08, 0.9, 0.9])    
     ax1.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-    ax1.annotate('Unstable', xy=(0.4, 1.06), xycoords='axes fraction', **font_ttl)
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax1.plot(vU, xcd_u[1,:,jtv,ja], linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75 )
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    locs, labels = plt.xticks() ; jl=0; newlabels = []
-    for ll in locs:
-        if not jl % 2 == 0: clab = ''
-        else: clab = str(int(locs[jl]))
-        newlabels.append(clab); jl=jl+1
-    plt.xticks(locs,newlabels)
-    ax1.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_cd_u_min, F_cd_u_max+dCd_u, dCd_u) ; plt.yticks( vy_ticks )
-    ax1.set_ylim(F_cd_u_min, F_cd_u_max + dCd_u*0.8)
-    ax1.annotate('a)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-    ctit = r'$C_D$'
+    ax1.annotate(cstblt, xy=(0.4, 1.06), xycoords='axes fraction', **font_ttl)
+    #
+    for ja in range(nba):
+        plt.scatter( xF1[1,:,:,ja], xF2[1,:,:,ja], marker='.', label=valg_dn[ja] )
+    #for jtv in range(nvt):
+    #    for ja in range(nba):
+    #        ax1.plot( xF1, xF2[1,:,jtv,ja], label=valg_dn[ja]+r' $\Delta\Theta=$'+csgn+str(vstab[jtv])+'K', \
+    #                  linewidth=3.5-jtv*0.7, color=vcolor[ja], linestyle=vlines[ja], zorder=0.75 )
+    #vx_ticks = nmp.arange(w_min, w_max+dw, dw)
+    #plt.xticks( vx_ticks )
+    #locs, labels = plt.xticks() ; jl=0; newlabels = []
+    #for ll in locs:
+    #    if not jl % 2 == 0: clab = ''
+    #    else: clab = str(int(locs[jl]))
+    #    newlabels.append(clab); jl=jl+1
+    #plt.xticks(locs,newlabels)
+
+    #print 'shape( f1_rng) = ', nmp.shape( f1_rng)
+    
+    if nmp.shape(f1_rng) == (3,):
+        vx_ticks = nmp.arange(f1_rng[0], f1_rng[1]+f1_rng[2], f1_rng[2]) ; plt.xticks( vx_ticks )
+        ax1.set_xlim(f1_rng[0], f1_rng[1])
+    #
+    if nmp.shape(f2_rng) == (3,):
+        vy_ticks = nmp.arange(f2_rng[0], f2_rng[1]+f2_rng[2], f2_rng[2]) ; plt.yticks( vy_ticks )
+        ax1.set_ylim(f2_rng[0], f2_rng[1])
+    #
+    ctit = r'$'+cvar_nm+'$ --'+cstblt+'--'
     plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax1.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-    if lshow_xylabs: plt.ylabel(r'$(10^{-3})$', **font_ylb)
-
-
-    ########################################################################
-    ax2  = plt.axes([0.5+rleft, 2*rheig+3*rbott+rbext, rwidt, rheig])
-    ax2.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-    ax2.annotate('Stable', xy=(0.42, 1.06), xycoords='axes fraction', **font_ttl)
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax2.plot(vU, xcd_s[1,:,jtv,ja], label=valgo_DN[ja]+r'$\Delta\Theta=\pm$'+str(vrt_s[jtv])+'K', linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75)
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    plt.xticks(locs,newlabels)
-    ax2.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_cd_s_min, F_cd_s_max+dCd_s, dCd_s) ; plt.yticks( vy_ticks )
-    ax2.set_ylim(F_cd_s_min, F_cd_s_max + dCd_s/2)
-    ax2.annotate('b)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-    ctit = r'$C_D$'
-    plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax2.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-
-
-
-    ############## C_H ###################
-    ax3  = plt.axes([rleft, rheig+2*rbott+rbext, rwidt, rheig])
-    ax3.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax3.plot(vU, xch_u[1,:,jtv,ja], label=valgo_DN[ja]+r'$\Delta\Theta=$'+str(vrt_u[jtv]),  linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75 )
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    plt.xticks(locs,newlabels)
-    ax3.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_ch_u_min, F_ce_u_max+dCe, dCe) ; plt.yticks( vy_ticks )
-    ax3.set_ylim(F_ch_u_min, F_ch_u_max + dCe/2.)
-    ax3.annotate('c)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-
-    ctit = r'$C_H$'
-    plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax3.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-    if lshow_xylabs: plt.ylabel(r'$(10^{-3})$', **font_ylb)
-    ax4  = plt.axes([0.5+rleft, rheig+2*rbott+rbext, rwidt, rheig])
-    ax4.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax4.plot(vU, xch_s[1,:,jtv,ja], label=valgo_DN[ja]+r'$\Delta\Theta=$'+str(vrt_s[jtv]),  linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75 )
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    plt.xticks(locs,newlabels)
-    ax4.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_ch_s_min, F_ce_s_max+dCe, dCe) ; plt.yticks( vy_ticks )
-    ax4.set_ylim(F_ch_s_min, F_ch_s_max + dCe*0.8)
-    ax4.annotate('d)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-    ctit = r'$C_H$'
-    plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax4.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-
-
-    ############## C_E ###################
-    ax5  = plt.axes([rleft, rbott+rbext, rwidt, rheig])
-    ax5.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax5.plot(vU, xce_u[1,:,jtv,ja], label=valgo_DN[ja]+r'$\Delta\Theta=$'+str(vrt_u[jtv]),  linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75 )
-
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    plt.xticks(locs,newlabels)
-
-    ax5.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_ce_u_min, F_ce_u_max+dCe, dCe) ; plt.yticks( vy_ticks )
-    ax5.set_ylim(F_ce_u_min, F_ce_u_max + dCe/2.)
-
-    ax5.annotate('e)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-
-    if lshow_xylabs: plt.xlabel(r'$U_{10m}$', **font_xlb)
-
-    ctit = r'$C_E$'
-    plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax5.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-    if lshow_xylabs: plt.ylabel(r'$(10^{-3})$', **font_ylb)
-    ax6  = plt.axes([0.5+rleft, rbott+rbext, rwidt, rheig])
-
-    ax6.grid(color='k', linestyle='-', linewidth=0.2, zorder=0.1)
-
-    for jtv in range(ntv):
-        for ja in range(nb_algo):
-            ax6.plot(vU, xce_s[1,:,jtv,ja], label=valgo_DN[ja]+r'$\Delta\Theta=\pm$'+str(vrt_s[jtv])+'K', linewidth=3.5-jtv*0.7, color=vcolor[ja], zorder=0.75 )
-
-    vx_ticks = nmp.arange(U_min, U_max+dU, dU)
-    plt.xticks( vx_ticks )
-    plt.xticks(locs,newlabels)
-
-    ax6.set_xlim(U_min, U_max)
-    vy_ticks = nmp.arange(F_ce_s_min, F_ce_s_max+dCe, dCe) ; plt.yticks( vy_ticks )
-    ax6.set_ylim(F_ce_s_min, F_ce_s_max + dCe*0.8)
-    ax6.annotate('f)', xy=xy_labs, xycoords='axes fraction', **font_lab)
-    if lshow_xylabs: plt.xlabel(r'$U_{10m}$', **font_xlb)
-    ctit = r'$C_E$'
-    plt.text(0.5, y_tit, ctit, horizontalalignment='center', transform = ax6.transAxes, bbox=dict(boxstyle="square", fc='w'), **font_ttl)
-    #plt.legend(loc='lower left', ncol=3, shadow=False, fancybox=True)
-    plt.legend(bbox_to_anchor=(0.6, -0.22), ncol=3, shadow=False, fancybox=True)
-
-
-    plt.savefig(fig_dir+'/'+cf_fig+'.'+fig_ext, dpi=DPI_FIG, facecolor='w', edgecolor='w', orientation='portrait')
+    plt.ylabel(r''+cyunit, **font_ylb)
+    plt.xlabel(r''+cxunit, **font_xlb)
+    plt.legend(bbox_to_anchor=(0.34, 0.16), ncol=3, shadow=False, fancybox=True)
+    #
+    plt.savefig(fig_dir+'/'+cvar_nm+'_'+cstblt+'.'+fig_ext, dpi=DPI_FIG, facecolor='w', edgecolor='w', orientation='portrait')
     plt.close(1)
+    return 0
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+# u*(UN10)    (must be more or less identical for stable and unstable !!!, but good to keep boths for debugging purposes...)
+iplt = plot_scat( nb_algo, 'u*(UN10)', xun_u, xus_u, valgo_DN, f1_rng=[0,30,1], f2_rng=[0,1.5,0.1], istab=0, cxunit='$U_{N10}$ [m/s]', cyunit='u* [m/s]' )
+iplt = plot_scat( nb_algo, 'u*(UN10)', xun_s, xus_s, valgo_DN, f1_rng=[0,30,1], f2_rng=[0,1.5,0.1], istab=1, cxunit='$U_{N10}$ [m/s]', cyunit='u* [m/s]' )
+
+
+
+
+
+
+
+# x-axis is now neutral-stability wind speed:
+
+
+
+
 
 
 
