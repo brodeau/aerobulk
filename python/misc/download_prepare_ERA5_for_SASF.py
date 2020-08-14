0b40dfde8151f1cd0cf7649f6f08b9b80de13bda
brodeau <brodeau@gmail.com>
2020-08-14 16:51:35 +0200

    LB

--- python/misc/download_prepare_ERA5_for_SASF.py
+++ python/misc/download_prepare_ERA5_for_SASF.py
@@ -9,11 +9,13 @@
 import sys
 from os import path
 import cdsapi
-from netCDF4 import Dataset
+from netCDF4 import Dataset,num2date
 from math import copysign
 import numpy as nmp
 
 yyyy = 2018
+
+l_interp_daily = True
 
 # Coordinates (point) we want to extract for STATION ASF:
 
@@ -72,6 +74,7 @@ def long_to_m180_p180(xx):
 # Coordinates of 10deg-wide box to download:
 dw = 5. ; # degrees
 irng_lon = [ int(round(long_to_m180_p180(plon-dw),0)) , int(round(long_to_m180_p180(plon+dw),0)) ]
+
 irng_lat = [ int(round(max(plat-dw,-90.),0))          , int(round(min(plat+dw, 90.),0))          ]
 
 plon = long_to_m180_p180(plon)
@@ -79,38 +82,45 @@ plon = long_to_m180_p180(plon)
 print(' * Longitude =', plon, ' =>', irng_lon[:])
 print(' * Latitude  =', plat, ' =>', irng_lat[:])
 print('')
+cinfo_box5x5 = str(irng_lat[1])+'-'+str(irng_lat[0])+'_'+str(irng_lon[0])+'-'+str(irng_lon[1])
+cinfo_coord  = str(plat)+'N_'+str(plon)+'E'
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-# Downloading daily fields, month after month...
-
+# Monthly stuff:
 nbfld_daily = len(list_var_daily_expected)
 if len(lvdl_daily) != nbfld_daily:
     print(' ERROR: download list "lvdl_daily" not the same size as "list_var_daily_expected"!!!', len(lvdl_daily), nbfld_daily) ; sys.exit(0)
 
+# Hourly stuff:
+nbfld = len(list_var_expected)
+if len(lvdl) != nbfld:
+    print(' ERROR: download list "lvdl" not the same size as "list_var_expected"!!!', len(lvdl), nbfld)
+    print(lvdl,'\n')
+    print(list_var_expected,'\n')
+    sys.exit(0)
 
+nbfld_tot = nbfld
+if l_interp_daily: nbfld_tot = nbfld + nbfld_daily
 
+    
+
+Nt_tot_daily = 0
+    
 c = cdsapi.Client()
 
 for jm in range(12):
 
     cm = '%2.2i'%(jm+1)
 
-    cf_fi_d = 'ERA5_arctic_surface_'+str(irng_lat[1])+'-'+str(irng_lat[0])+'_'+str(irng_lon[0])+'-'+str(irng_lon[1])+'_'+str(yyyy)+cm+'_daily.nc'
-    cf_fo_d = 'ERA5_arctic_surface_'+str(plat)+'N_'+str(plon)+'E_1d_y'+str(yyyy)+'m'+cm+'.nc'
+    cf_fi_d = 'ERA5_arctic_surface__BOX-5x5deg_'+cinfo_box5x5+'__'+str(yyyy)+cm+'_daily.nc'
+    #cf_fo_d = 'ERA5_arctic_surface_'+cinfo_coord+'_1d_y'+str(yyyy)+'m'+cm+'.nc'
+
+    print(' *** cf_fi_d = '+cf_fi_d)
+    #print(' *** cf_fo_d = '+cf_fo_d)
     
     if not path.exists(cf_fi_d):
     
@@ -210,34 +220,102 @@ for jm in range(12):
         #if not id_fi.dimensions['time'].isunlimited(): print 'PROBLEM: the time dimension is not UNLIMITED! Bad!'; sys.exit(0)
         Nt = id_fi.dimensions['time'].size ; # Not unlimited in downloaded files...
         print(' *** Input file: Ni, Nj, Nt = ', Ni, Nj, Nt, '\n')
+        Nt_tot_daily = Nt_tot_daily + Nt
+        #vtime_d = id_fi.variables['time'][:]
+        
         id_fi.close()
 
 
 
+# The 12 month for the daily field have been downloaded
+print('\n Number of days for year '+str(yyyy)+': '+str(Nt_tot_daily-2))
+
+xdata_d = nmp.zeros((nbfld_daily,Nt_tot_daily))  ; # before and after
+vtime_d = nmp.zeros(             Nt_tot_daily )
+
+
+
+# Reading the 12 files and filling the vtime_d and xdata_d (whole time-series for current year):
+jt = 0
+for jm in range(12):
+    cm = '%2.2i'%(jm+1)
+    cf_fi_d = 'ERA5_arctic_surface__BOX-5x5deg_'+cinfo_box5x5+'__'+str(yyyy)+cm+'_daily.nc'
+
+    if cm == '01':
+        id_fi = Dataset(cf_fi_d+'.before')
+        vt = id_fi.variables['time'][:]
+        if len(vt) != 1: print('ERROR #1!'); sys.exit(1)
+        if jt==0: cunit_t = id_fi.variables['time'].units
+        #
+        vlon  = id_fi.variables['longitude'][:]
+        vlat  = id_fi.variables['latitude'][:]
+        ip = nmp.argmin(nmp.abs(vlon-plon))
+        jp = nmp.argmin(nmp.abs(vlat-plat))
+        print(' *** ip, jp =', ip, jp)
+        #
+        vtime_d[jt] = vt[0]
+        jv = 0
+        for cv in list_var_daily_expected:
+            xdata_d[jv,jt] = id_fi.variables[cv][0,jp,ip]
+            jv=jv+1
+        id_fi.close()
+        jt = jt + 1
+
+    # Always:
+    id_fi = Dataset(cf_fi_d)
+    vt = id_fi.variables['time'][:]
+    Nt = len(vt)
+    vtime_d[jt:Nt+jt] = vt[:]
+    jv = 0
+    for cv in list_var_daily_expected:
+        xdata_d[jv,jt:Nt+jt] = id_fi.variables[cv][:,jp,ip]
+        jv=jv+1
+    id_fi.close()
+    jt = jt + Nt
+
+    if cm == '12':
+        id_fi = Dataset(cf_fi_d+'.after')
+        vt = id_fi.variables['time'][:]
+        if len(vt) != 1: print('ERROR #1!'); sys.exit(2)
+        vtime_d[jt] = vt[0]
+        jv = 0
+        for cv in list_var_daily_expected:
+            xdata_d[jv,jt] = id_fi.variables[cv][0,jp,ip]
+            jv=jv+1
+        id_fi.close()
+        jt = jt + 1
+
+
+print('\n jt, Nt_tot_daily =', jt, Nt_tot_daily)
+
+# Debug to control...
+print('\n\n')
+for jt in range(Nt_tot_daily):
+    print(jt, vtime_d[jt], num2date(vtime_d[jt], units=cunit_t))
+print('\n\n')
+
+
+## ===> so vtime_d[:] and xdata_d[:,:] is what needs to be interpolated later !
 
 
 
 
 
-# Downloading hourly fields, month after month...
-
-nbfld = len(list_var_expected)
-if len(lvdl) != nbfld:
-    print(' ERROR: download list "lvdl" not the same size as "list_var_expected"!!!', len(lvdl), nbfld)
-    print(lvdl,'\n')
-    print(list_var_expected,'\n')
-    sys.exit(0)
 
 
 
-c = cdsapi.Client()
 
+
+#################
+# Hourly fields #
+#################
+    
 for jm in range(12):
 
     cm = '%2.2i'%(jm+1)
-
-    cf_fi = 'ERA5_arctic_surface_'+str(irng_lat[1])+'-'+str(irng_lat[0])+'_'+str(irng_lon[0])+'-'+str(irng_lon[1])+'_'+str(yyyy)+cm+'.nc'
-    cf_fo = 'ERA5_arctic_surface_'+str(plat)+'N_'+str(plon)+'E_1h_y'+str(yyyy)+'m'+cm+'.nc'
+    
+    cf_fi = 'ERA5_arctic_surface__BOX-5x5deg_'+cinfo_box5x5+'__'+str(yyyy)+cm+'.nc'
+    cf_fo = 'ERA5_arctic_surface_'+cinfo_coord+'_1h_y'+str(yyyy)+'m'+cm+'.nc'
     
     if not path.exists(cf_fi):
     
@@ -249,7 +327,9 @@ for jm in range(12):
                 'product_type': 'reanalysis',
                 'format': 'netcdf',
                 'variable': [
-                     lvdl[0], lvdl[1], lvdl[2], lvdl[3], lvdl[4], lvdl[5], lvdl[6], lvdl[7], lvdl[8], lvdl[9], lvdl[10], lvdl[11],
+                    lvdl[0], lvdl[1], lvdl[2], lvdl[3],
+                    lvdl[4], lvdl[5], lvdl[6], lvdl[7],
+                    lvdl[8], lvdl[9], lvdl[10], lvdl[11],
                 ],
                 'year': str(yyyy),
                 'month': [
@@ -321,6 +401,8 @@ for jm in range(12):
     # Creating output file for ocean:
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ni = 3 ; nj = 3
+    jr1 = 0 ; jr2 = 0
+    
     id_fo = Dataset(cf_fo, 'w', format='NETCDF4')
 
     # Dimensions:
@@ -336,7 +418,7 @@ for jm in range(12):
     # Creating fields in output file:
     ido_var = []
     iv = 0
-    for cvar in list_var_expected:        
+    for cvar in list_var_expected:
         ido_var.append(id_fo.createVariable(cvar, 'f4', (cv_tim,'y','x',), zlib=True))
         if cvar in list_flx:
             idx = list_flx.index(cvar)
@@ -348,11 +430,19 @@ for jm in range(12):
         ido_var[iv].long_name = id_fi.variables[cvar].long_name
         iv = iv + 1
 
+    # Dayly fields that will be interpolated in here:
+    if l_interp_daily:
+        for cvar in list_var_daily_expected:
+            ido_var.append(id_fo.createVariable(cvar, 'f4', (cv_tim,'y','x',), zlib=True))
+            ido_var[iv].units = 'boo'
+            ido_var[iv].long_name = 'boo'
+            iv = iv + 1
+        
     # Creating extra fields:
     for cvar in list_extra:
         ido_var.append(id_fo.createVariable(cvar, 'f4', (cv_tim,'y','x',), zlib=True))
-        ido_var[iv].units = cunt_extra[iv-nbfld]
-        ido_var[iv].long_name = clnm_extra[iv-nbfld]
+        ido_var[iv].units = cunt_extra[iv-nbfld_tot]
+        ido_var[iv].long_name = clnm_extra[iv-nbfld_tot]
         iv = iv + 1
         
     # Filling coordinates:
@@ -361,7 +451,10 @@ for jm in range(12):
 
     # Filling fields
     for jt in range(Nt):
-        ido_tim[jt] = vtime[jt]
+
+        rt = vtime[jt] ; # current time!        
+        ido_tim[jt] = rt
+        
         iv = 0
         for cvar in list_var_expected:
             ido_var[iv][jt,:,:] = id_fi.variables[cvar][jt,jp,ip]
@@ -374,8 +467,26 @@ for jm in range(12):
                 ido_var[iv][jt,:,:] = ido_var[iv][jt,:,:] - 273.15
             #
             iv = iv + 1
+
+        # Dayly fields that will be interpolated in here:
+        if l_interp_daily:
+            #
+            #print('LOLO: current time is rt =', rt)
+            for jtd in range(jr1, Nt_tot_daily-1):
+                if vtime_d[jtd] <= rt and vtime_d[jtd+1] > rt:
+                    jr1 = jtd ; jr2 = jtd + 1 
+                    break
+            #print('LOLO: what we found is:', vtime_d[jr1], vtime_d[jr2])
+            #
+            ivd = 0
+            for cvar in list_var_daily_expected:
+                # Linear interpolation !!!
+                rslope = (xdata_d[ivd,jr2] - xdata_d[ivd,jr1])/(vtime_d[jr2] - vtime_d[jr1])
+                ido_var[iv][jt,:,:] = xdata_d[ivd,jr1] +  rslope*(rt - vtime_d[jr1])
+                iv = iv + 1 ; ivd = ivd + 1
+                
         for cvar in list_extra:
-            ido_var[iv][jt,:,:] = rval_extra[iv-nbfld]
+            ido_var[iv][jt,:,:] = rval_extra[iv-nbfld_tot]
             iv = iv + 1
 
             
