8a512e721f03b2df4216b7bd2505bf993f697433
Laurent BRODEAU <lbrodeau@datarmor3.ib0.ice.ifremer.fr>
2021-01-21 07:53:53 +0000

    LB

--- python/misc/build_q2_from_d2_slp.py
+++ python/misc/build_q2_from_d2_slp.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # L. Brodeau, June 2019
 
@@ -9,26 +9,7 @@ from os.path import exists,basename
 
 import aerobulk_physf as abp
 
-
-iconv=2
-
-if   iconv == 1:
-    cv_d2='D2M'
-    cv_p0='MSL'
-    cv_q2='Q2M'
-    cv_lon = 'lon'
-    cv_lat = 'lat'
-    
-elif iconv == 2:
-    # ERA5...
-    cv_d2='d2m'
-    cv_p0='msl'
-    cv_q2='q2m'
-    cv_lon = 'lon'
-    cv_lat = 'lat'
-
 rmiss = -9999.
-
 
 def __chck4f__(cf, script_name=''):
     cmesg = 'ERROR: File '+cf+' does not exist !!!'
@@ -38,33 +19,17 @@ def __chck4f__(cf, script_name=''):
     else:
         print(' *** will open file '+cf)
 
-
-
-    
-    
 if not len(sys.argv) in [2,3]:
     print('Usage: '+sys.argv[0]+' <IN_FILE_D2.nc> (<lsm_file.nc>)')
     sys.exit(0)
 
+
 cf_d2   = sys.argv[1]
-cf_p0 = str.replace(cf_d2, cv_d2, cv_p0)
 
 l_mask = False
 if len(sys.argv) == 3:
     l_mask = True
     cf_lsm = sys.argv[2]
-
-
-cf_q2 = basename(str.replace(cf_d2, cv_d2, cv_q2))
-
-__chck4f__(cf_d2)
-__chck4f__(cf_p0)
-if l_mask:
-    __chck4f__(cf_lsm)
-    cf_q2 = basename(str.replace(cf_q2, '.nc', '_masked.nc'))
-
-print('\n *** Will generate file '+cf_q2+' !\n')
-
 
 if l_mask:
     print(' *** Opening land-sea mask file... '+cf_lsm)
@@ -80,24 +45,69 @@ if l_mask:
 
 # First need time length:
 f_d2_in = Dataset(cf_d2)
-vlon     = f_d2_in.variables[cv_lon][:]
-cunt_lon = f_d2_in.variables[cv_lon].units
-clnm_lon = f_d2_in.variables[cv_lon].long_name
+#
+list_var = f_d2_in.variables.keys()
+
+# Name of longitude and latitude ?
+cv_lon='0'
+for cv_lon in [ 'lon', 'longitude', 'LON', 'LONGITUDE', 'nav_lon', 'glamt' ]:
+    if cv_lon in list_var: break
+if cv_lon=='0': print("PROBLEM: could not find name of longitude variable!"); sys.exit(0)
+#
+cv_lat='0'
+for cv_lat in [ 'lat', 'latitude', 'LAT', 'LATITUDE', 'nav_lat', 'gphit' ]:
+    if cv_lat in list_var: break
+if cv_lat=='0': print("PROBLEM: could not find name of latitude variable!"); sys.exit(0)
+#
+cv_time='0'
+for cv_time in [ 'time', 'time_counter', 'TIME' ]:
+    if cv_time in list_var: break
+if cv_time=='0': print("PROBLEM: could not find name of time variable!"); sys.exit(0)
+#
+cv_d2='0'
+for cv_d2 in [ 'd2m', 'D2M' ]:
+    if cv_d2 in list_var: break
+if cv_d2=='0': print("PROBLEM: could not find name of d2 variable!"); sys.exit(0)
+if   cv_d2=='D2M':
+    cv_p0='MSL'
+    cv_q2='Q2M'
+elif cv_d2=='d2m':
+    cv_p0='msl'
+    cv_q2='q2m'
+
+cunt_lon='unknown' ; clnm_lon='Longitude'
+vlon    = f_d2_in.variables[cv_lon][:]
+lst_att = f_d2_in.variables[cv_lon].ncattrs()
+if 'units'     in lst_att: cunt_lon = f_d2_in.variables[cv_lon].units
+if 'long_name' in lst_att: clnm_lon = f_d2_in.variables[cv_lon].long_name
 print('LONGITUDE: ', cunt_lon, clnm_lon)
-
-# Extracting the longitude 1D array:
-vlat     = f_d2_in.variables[cv_lat][:]
-cunt_lat = f_d2_in.variables[cv_lat].units
-clnm_lat = f_d2_in.variables[cv_lat].long_name
+#
+cunt_lat='unknown' ; clnm_lat='Latitude'
+vlat    = f_d2_in.variables[cv_lat][:]
+lst_att = f_d2_in.variables[cv_lat].ncattrs()
+if 'units'     in lst_att: cunt_lat = f_d2_in.variables[cv_lat].units
+if 'long_name' in lst_att: clnm_lat = f_d2_in.variables[cv_lat].long_name
 print('LATITUDE: ', cunt_lat, clnm_lat)
-
-# Extracting time 1D array:
-vtime     = f_d2_in.variables['time'][:]
-cunt_time = f_d2_in.variables['time'].units
-#ccal_time = f_d2_in.variables['time'].calendar
+#
+cunt_time='unknown'
+vtime   = f_d2_in.variables[cv_time][:]
+lst_att = f_d2_in.variables[cv_time].ncattrs()
+if 'units' in lst_att: cunt_time = f_d2_in.variables[cv_time].units
 print('TIME: ', cunt_time, '\n')
+#
 f_d2_in.close()
 
+
+
+cf_p0 = str.replace(cf_d2, cv_d2, cv_p0)
+__chck4f__(cf_p0)
+
+cf_q2 = basename(str.replace(cf_d2, cv_d2, cv_q2))
+__chck4f__(cf_d2)
+if l_mask:
+    __chck4f__(cf_lsm)
+    cf_q2 = basename(str.replace(cf_q2, '.nc', '_masked.nc'))
+print('\n *** Will generate file '+cf_q2+' !\n')
 
 
 
@@ -115,7 +125,9 @@ for jt in range(Nt):
     # ~~~
     if jt == 0:
         f_d2_in = Dataset(cf_d2)
-        cunt_d2 = f_d2_in.variables[cv_d2].units
+        cunt_d2 = 'unknown'
+        lst_att = f_d2_in.variables[cv_d2].ncattrs()
+        if 'units' in lst_att: cunt_d2 = f_d2_in.variables[cv_d2].units
     xd2     = f_d2_in.variables[cv_d2][jt,:,:]
     if jt == Nt-1: f_d2_in.close()
     
@@ -124,7 +136,9 @@ for jt in range(Nt):
     # ~~~
     if jt == 0:
         f_p0_in = Dataset(cf_p0)
-        cunt_p0 = f_p0_in.variables[cv_p0].units
+        cunt_p0 = 'unknown'
+        lst_att = f_p0_in.variables[cv_p0].ncattrs()
+        if 'units' in lst_att: cunt_p0 = f_p0_in.variables[cv_p0].units
     xp0     = f_p0_in.variables[cv_p0][jt,:,:]
     if jt == Nt-1: f_p0_in.close()
                 
@@ -156,17 +170,17 @@ for jt in range(Nt):
         # Dimensions:
         f_out.createDimension(cv_lon, ni)
         f_out.createDimension(cv_lat, nj)
-        f_out.createDimension('time', None)
+        f_out.createDimension(cv_time, None)
     
         # Variables
         id_lon = f_out.createVariable(cv_lon,'f4',(cv_lon,),               zlib=True)
         id_lat = f_out.createVariable(cv_lat,'f4',(cv_lat,),               zlib=True)
-        id_tim = f_out.createVariable('time','f4',('time',),               zlib=True)
+        id_tim = f_out.createVariable(cv_time,'f4',(cv_time,),               zlib=True)
         
         if l_mask:
-            id_q2  = f_out.createVariable(cv_q2, 'f4',('time',cv_lat,cv_lon,), zlib=True, fill_value=rmiss)
+            id_q2  = f_out.createVariable(cv_q2, 'f4',(cv_time,cv_lat,cv_lon,), zlib=True, fill_value=rmiss)
         else:
-            id_q2  = f_out.createVariable(cv_q2, 'f4',('time',cv_lat,cv_lon,), zlib=True)
+            id_q2  = f_out.createVariable(cv_q2, 'f4',(cv_time,cv_lat,cv_lon,), zlib=True)
 
         # Attributes
         id_tim.units    = cunt_time
