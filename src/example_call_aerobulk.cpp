a27005bd92609090b769bc7c98b7b8574560f5d5
brodeau <brodeau@gmail.com>
2021-05-12 13:58:25 +0200

    LB

--- src/example_call_aerobulk.cpp
+++ src/example_call_aerobulk.cpp
@@ -6,26 +6,30 @@
 int main(int argc, char** argv)
 {
 
-    std::vector<double> zsst  = {273.15 + 22., 273.15 + 22.};
-    std::vector<double> zt_zt = {273.15 + 20., 273.15 + 25.}; // second case is stable ABL as t_air > SST (25>22)!
-    std::vector<double> zq_zt = {0.012, 0.012};
-    std::vector<double> zU_zu = { 4.,  4.};
-    std::vector<double> zV_zu = {10., 10.};
-    std::vector<double> zslp  = {101000.0, 101000.0};
+    double zt =  2.; // height of measurement for air temperature and humidity [m]
+    double zu = 10.; // height of measurement for wind speed [m]
 
-    std::vector<double> zRsw  = {0., 0.}; // (night)
-    std::vector<double> zRlw  = {350., 350.};
+    std::vector<double> zsst  = {273.15 + 22., 273.15 + 22.}; // sea surface temperature [K]
+    std::vector<double> zt_zt = {273.15 + 20., 273.15 + 25.}; // air absolute temperature at zt [K] ## second case is stable ABL as t_air > SST (25>22)!
+    std::vector<double> zq_zt = {0.012, 0.012};               // air specific humidity at zt [g/kg]
+    std::vector<double> zU_zu = { 4.,  4.};                   // u-wind speed component at zu [m/s]
+    std::vector<double> zV_zu = {10., 10.};                   // v-wind speed component at zu [m/s]
+    std::vector<double> zslp  = {101000.0, 101000.0};         // sea-level atmospheric pressure [Pa]
 
+    std::vector<double> zRsw  = {0., 0.};       // downwelling shortwave (solar)     radiation [W/m^2] ## night!
+    std::vector<double> zRlw  = {350., 350.};   // downwelling longwave  (infra-red) radiation [W/m^2] 
+
+    // AeroBulk output:
     std::vector<double> zQL;
     std::vector<double> zQH;
     std::vector<double> zTau_x;
     std::vector<double> zTau_y;
     std::vector<double> zT_s;
 
-    aerobulk::model(aerobulk::algorithm::COARE3p6, 2, 10, zsst, zt_zt,
-            zq_zt, zU_zu, zV_zu, zslp,
-            zQL, zQH, zTau_x, zTau_y,
-            zRsw, zRlw, zT_s );
+    aerobulk::model(aerobulk::algorithm::COARE3p6, zt, zu, zsst, zt_zt,
+                    zq_zt, zU_zu, zV_zu, zslp,
+                    zQL, zQH, zTau_x, zTau_y,
+                    zRsw, zRlw, zT_s );
 
     std::cout
         << "\n *********** COARE 3.6 *****************\n"
@@ -40,10 +44,10 @@ int main(int argc, char** argv)
     std::cout << " Evaporation = \t" << -zQL[0]*1e-3/L[0] << "\t" << -zQL[1]*1e-3/L[1] << std::endl;
 
 
-    aerobulk::model(aerobulk::algorithm::ECMWF, 2, 10, zsst, zt_zt,
-            zq_zt, zU_zu, zV_zu, zslp,
-            zQL, zQH, zTau_x, zTau_y,
-            zRsw, zRlw, zT_s );
+    aerobulk::model(aerobulk::algorithm::ECMWF, zt, zu, zsst, zt_zt,
+                    zq_zt, zU_zu, zV_zu, zslp,
+                    zQL, zQH, zTau_x, zTau_y,
+                    zRsw, zRlw, zT_s );
 
     std::cout
         << "\n *********** ECMWF *****************\n"
