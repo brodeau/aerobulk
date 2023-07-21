e8ff50c3bec19e8d298706b64d600e482901dac2
brodeau <brodeau@gmail.com>
2023-07-21 12:01:17 +0200

    LB

--- src/ice/mod_blk_ice_easy.f90
+++ src/ice/mod_blk_ice_easy.f90
@@ -36,10 +36,10 @@ MODULE mod_blk_ice_easy
    PRIVATE
 
    PUBLIC :: TURB_ICE_EASY
-   
+
    !!----------------------------------------------------------------------
 CONTAINS
-   
+
    SUBROUTINE turb_ice_easy( zt, zu, Ts_i, t_zt, qs_i, q_zt, U_zu,     &
       &                      CdN, ChN, CeN,                            &
       &                      Cd_i, Ch_i, Ce_i, t_zu_i, q_zu_i, Ubzu,   &
@@ -95,7 +95,7 @@ CONTAINS
       REAL(wp), INTENT(in ), DIMENSION(:,:) :: U_zu  ! relative wind module at zu                [m/s]
       REAL(wp), INTENT(out), DIMENSION(:,:) :: CdN
       REAL(wp), INTENT(out), DIMENSION(:,:) :: ChN
-      REAL(wp), INTENT(out), DIMENSION(:,:) :: CeN      
+      REAL(wp), INTENT(out), DIMENSION(:,:) :: CeN
       REAL(wp), INTENT(out), DIMENSION(:,:) :: Cd_i  ! drag coefficient over sea-ice
       REAL(wp), INTENT(out), DIMENSION(:,:) :: Ch_i  ! transfert coefficient for heat over ice
       REAL(wp), INTENT(out), DIMENSION(:,:) :: Ce_i  ! transfert coefficient for sublimation over ice
@@ -108,13 +108,13 @@ CONTAINS
       REAL(wp), INTENT(out), DIMENSION(:,:), OPTIONAL :: xL  ! zeta (zu/L)
       REAL(wp), INTENT(out), DIMENSION(:,:), OPTIONAL :: xUN10  ! Neutral wind at zu
       !!----------------------------------------------------------------------------------
-      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: ztmp0, ztmp1, ztmp2      ! temporary stuff
-      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: z0, dt_zu, dq_zu
+      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: ztmp0, ztmp1      ! temporary stuff
+      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: dt_zu, dq_zu
       REAL(wp), DIMENSION(:,:), ALLOCATABLE :: u_star, t_star, q_star
-      REAL(wp), DIMENSION(:,:), ALLOCATABLE :: znu_a                    !: Nu_air = kinematic viscosity of air
       REAL(wp), DIMENSION(:,:), ALLOCATABLE :: zeta_u, zeta_t           ! stability parameter at height zu
-      REAL(wp), DIMENSION(:,:,:), ALLOCATABLE :: z0tq
       !!
+      REAL(wp) :: zsqrtCDN, zlog1, zlog2
+
       INTEGER :: Ni, Nj, jit
       LOGICAL :: l_zt_equal_zu = .FALSE.      ! if q and t are given at same height as U
       !!
@@ -124,11 +124,10 @@ CONTAINS
       !!----------------------------------------------------------------------------------
       Ni = SIZE(Ts_i,1)
       Nj = SIZE(Ts_i,2)
-      
+
       ALLOCATE ( u_star(Ni,Nj), t_star(Ni,Nj),  q_star(Ni,Nj),  &
          &       zeta_u(Ni,Nj),  dt_zu(Ni,Nj),   dq_zu(Ni,Nj),  &
-         &        znu_a(Ni,Nj),  ztmp1(Ni,Nj),   ztmp2(Ni,Nj),  &
-         &           z0(Ni,Nj),   z0tq(Ni,Nj,2), ztmp0(Ni,Nj)   )
+         &       ztmp0(Ni,Nj),   ztmp1(Ni,Nj)  )
 
       lreturn_z0    = PRESENT(xz0)
       lreturn_ustar = PRESENT(xu_star)
@@ -138,90 +137,78 @@ CONTAINS
       l_zt_equal_zu = ( ABS(zu - zt) < 0.01_wp )
       IF( .NOT. l_zt_equal_zu )  ALLOCATE( zeta_t(Ni,Nj) )
 
+
+
+      zsqrtCDN = SQRT(CdN)
+
+      zlog1 = LOG(zt/zu)
+      zlog2 = LOG(zu/10._wp)
+
       !! Scalar wind speed cannot be below 0.2 m/s
-      Ubzu = MAX( U_zu, wspd_thrshld_ice )
+      Ubzu(:,:) = MAX( U_zu, wspd_thrshld_ice )
 
       !! First guess of temperature and humidity at height zu:
       t_zu_i = MAX( t_zt ,   100._wp )   ! who knows what's given on masked-continental regions...
       q_zu_i = MAX( q_zt , 0.1e-6_wp )   !               "
 
-      !! Air-Ice differences (and we don't want it to be 0!)
-      dt_zu = t_zu_i - Ts_i ;   dt_zu = SIGN( MAX(ABS(dt_zu),1.E-6_wp), dt_zu )
-      dq_zu = q_zu_i - qs_i ;   dq_zu = SIGN( MAX(ABS(dq_zu),1.E-9_wp), dq_zu )
-
-      znu_a = visc_air(t_zu_i) ! Air viscosity (m^2/s) at zt given from temperature in (K)
-
-      !! Very crude first guesses of z0:
-      z0 = 8.0E-4_wp
-
-      !! Crude first guess of turbulent scales
-      u_star = 0.035_wp*Ubzu*LOG( 10._wp/z0 )/LOG( zu/z0 )
-      z0 = rough_leng_m( u_star , znu_a )
-
-      DO jit = 1, 2
-         u_star = MAX ( Ubzu*vkarmn/(LOG(zu) - LOG(z0)) , 1.E-9 )
-         z0 = rough_leng_m( u_star , znu_a )
-      END DO
-
-      z0tq = rough_leng_tq( z0, u_star , znu_a )
-      t_star = dt_zu*vkarmn/(LOG(zu/z0tq(:,:,1)))
-      q_star = dq_zu*vkarmn/(LOG(zu/z0tq(:,:,2)))
-
+      !! First guess of transfer coefficients:
+      Cd_i(:,:) = CdN
+      Ch_i(:,:) = ChN
+      Ce_i(:,:) = CeN
 
 
       !! ITERATION BLOCK
       DO jit = 1, nb_iter
 
+         !! Air-Ice differences:
+         dt_zu(:,:) = t_zu_i - Ts_i ; !  dt_zu = SIGN( MAX(ABS(dt_zu),1.E-6_wp), dt_zu ) ; !RM 2nd part!!!
+         dq_zu(:,:) = q_zu_i - qs_i ; !  dq_zu = SIGN( MAX(ABS(dq_zu),1.E-9_wp), dq_zu )
+         
+         !! Now we can get the urbulent scales:
+         ztmp0(:,:)  = SQRT(Cd_i(:,:)) ; ! == u*/Ubzu
+         u_star = ztmp0(:,:) * Ubzu(:,:)
+         ztmp0(:,:)  = 1. / MAX(ztmp0(:,:), 1.E-15) ; ! == Ubzu/u*
+         t_star =      Ch_i(:,:)  * dt_zu(:,:) * ztmp0(:,:)
+         q_star =      Ce_i(:,:)  * dq_zu(:,:) * ztmp0(:,:)
+
          !!Inverse of Obukov length (1/L) :
-         ztmp0 = One_on_L(t_zu_i, q_zu_i, u_star, t_star, q_star)  ! 1/L == 1/[Obukhov length]
-         ztmp0 = SIGN( MIN(ABS(ztmp0),200._wp), ztmp0 ) ! (prevents FPE from stupid values from masked region later on...)
-
+         ztmp0(:,:) = One_on_L(t_zu_i, q_zu_i, u_star, t_star, q_star)  ! 1/L == 1/[Obukhov length]
+         ztmp0(:,:) = SIGN( MIN(ABS(ztmp0(:,:)),200._wp), ztmp0(:,:) ) ! (prevents FPE from stupid values from masked region later on...)
+         
          !! Stability parameters "zeta" :
-         zeta_u = zu*ztmp0
-         zeta_u = SIGN( MIN(ABS(zeta_u),50.0_wp), zeta_u )
+         zeta_u(:,:) = zu*ztmp0(:,:)
+         zeta_u(:,:) = SIGN( MIN(ABS(zeta_u(:,:)),50.0_wp), zeta_u(:,:) )
          IF( .NOT. l_zt_equal_zu ) THEN
-            zeta_t = zt*ztmp0
-            zeta_t = SIGN( MIN(ABS(zeta_t),50.0_wp), zeta_t )
+            zeta_t(:,:) = zt*ztmp0(:,:)
+            zeta_t(:,:) = SIGN( MIN(ABS(zeta_t(:,:)),50.0_wp), zeta_t(:,:) )
          END IF
+         
+         !! Update C_D:
+         ztmp0(:,:) = 1._wp + zsqrtCDN/vkarmn*(zlog2 - psi_m_ice(zeta_u(:,:)))
+         Cd_i(:,:)  = MAX( CdN / ( ztmp0(:,:)*ztmp0(:,:) ), Cx_min )
 
-         !! Roughness lengthes z0, z0t, & z0q :
-         z0   = rough_leng_m (     u_star , znu_a )
-         z0tq = rough_leng_tq( z0, u_star , znu_a )
-
-         !! Turbulent scales at zu :
-         ztmp0   = psi_h_ice(zeta_u)
-         t_star =      dt_zu*vkarmn/(LOG(zu) - LOG(z0tq(:,:,1)) - ztmp0)
-         q_star =      dq_zu*vkarmn/(LOG(zu) - LOG(z0tq(:,:,2)) - ztmp0)
-         u_star = MAX( Ubzu*vkarmn/(LOG(zu) - LOG(z0(:,:)) - psi_m_ice(zeta_u)) , 1.E-9 )
+         !! Update C_H and C_E
+         ztmp0(:,:) = ( zlog2 - psi_h_ice(zeta_u) ) / vkarmn / zsqrtCDN
+         ztmp1(:,:) = SQRT(Cd_i(:,:)) / zsqrtCDN
+         Ch_i(:,:)  = MAX( ChN*ztmp1(:,:) / ( 1._wp + ChN*ztmp0(:,:) ) , Cx_min )
+         Ce_i(:,:)  = MAX( CeN*ztmp1(:,:) / ( 1._wp + CeN*ztmp0(:,:) ) , Cx_min )
 
          IF( .NOT. l_zt_equal_zu ) THEN
             !! Re-updating temperature and humidity at zu if zt /= zu :
-            ztmp1 = LOG(zt/zu) + ztmp0 - psi_h_ice(zeta_t)
-            t_zu_i = t_zt - t_star/vkarmn*ztmp1
-            q_zu_i = q_zt - q_star/vkarmn*ztmp1
-            dt_zu = t_zu_i - Ts_i ;  dt_zu = SIGN( MAX(ABS(dt_zu),1.E-6_wp), dt_zu )
-            dq_zu = q_zu_i - qs_i ;  dq_zu = SIGN( MAX(ABS(dq_zu),1.E-9_wp), dq_zu )
+            ztmp0(:,:) = psi_h_ice(zeta_u(:,:)) - psi_h_ice(zeta_t(:,:)) + zlog1
+            t_zu(:,:)  =            t_zt(:,:) - t_star(:,:)/vkarmn*ztmp0(:,:)
+            q_zu(:,:)  = MAX(0._wp, q_zt(:,:) - q_star(:,:)/vkarmn*ztmp0(:,:) )
          END IF
-
+         
       END DO !DO jit = 1, nb_iter
 
-      ! compute transfer coefficients at zu :
-      ztmp0 = u_star/Ubzu
-      Cd_i   = ztmp0*ztmp0
-      Ch_i   = ztmp0*t_star/dt_zu
-      Ce_i   = ztmp0*q_star/dq_zu
-
-      !IF( lreturn_cdn .OR. lreturn_chn .OR. lreturn_cen ) ztmp0 = 1._wp/LOG( zu/z0(:,:) )
-      !IF( lreturn_cdn )   CdN = vkarmn2*ztmp0*ztmp0
-      !IF( lreturn_chn )   ChN = vkarmn2*ztmp0/LOG(zu/z0tq(:,:,1))
-      !IF( lreturn_cen )   CeN = vkarmn2*ztmp0/LOG(zu/z0tq(:,:,2))
-
-      IF( lreturn_z0 )    xz0     = z0
+      
+      IF( lreturn_z0 )    xz0 = z0_from_Cd( zu, Cd_i(:,:), ppsi=psi_m_ice(zeta_u(:,:)) )
       IF( lreturn_ustar ) xu_star = u_star
       IF( lreturn_L )     xL      = 1./One_on_L(t_zu_i, q_zu_i, u_star, t_star, q_star)
-      IF( lreturn_UN10 )  xUN10   = u_star/vkarmn*LOG(10./z0)
+      IF( lreturn_UN10 )  xUN10   = UN10_from_CD(zu, Ubzu(:,:), Cd_i(:,:), ppsi=psi_m_ice(zeta_u(:,:)) )
 
-      DEALLOCATE ( u_star, t_star, q_star, zeta_u, dt_zu, dq_zu, z0, z0tq, znu_a, ztmp0, ztmp1, ztmp2 )
+      DEALLOCATE ( u_star, t_star, q_star, zeta_u, dt_zu, dq_zu, ztmp0, ztmp1 )
       IF( .NOT. l_zt_equal_zu ) DEALLOCATE( zeta_t )
 
    END SUBROUTINE turb_ice_easy
